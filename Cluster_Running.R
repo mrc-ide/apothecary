71ea5d29a39296e6e4d9579f2542fbc028b0239b
CharlieWhittaker <cwhittaker1000@gmail.com>
2020-09-28 20:33:01 +0100

    cluster running updates

--- Cluster_Running.R
+++ Cluster_Running.R
@@ -4,27 +4,28 @@
 # install.packages("didehpc")
 # remotes::install_github("mrc-ide/provisionr@dev", upgrade = FALSE)
 # devtools::install_github("mrc-ide/buildr", upgrade=FALSE)
+# countries <- c("AFG", "ARG", "CAN", "DOM", "ETH", "GTM", "HND", "IDN",  "PAK", "ROU", "RUS", "UKR")
 
 # Setting Up Cluster
-loc <- didehpc::path_mapping("location", "M:", "//fi--didef3.dide.ic.ac.uk/malaria", "M:")
+loc <- didehpc::path_mapping("location", "N:", "//fi--didenas5/malaria", "N:")
 config <- didehpc::didehpc_config(shares = loc, use_rrq = FALSE, cluster = "fi--didemrchnb",
-                                  parallel = FALSE, template = "12Core", cores = 12, rtools = TRUE)
+                                  parallel = FALSE, rtools = TRUE)
 packages <- c("lubridate", "dplyr", "plyr", "tidyr", "odin", "squire", "apothecary", "dde")
 
 
 # Creating a Context
 sources <- c("MCMC_cluster_function.R")
 additional_identifier <- ""
-context_name <- paste0("M:/Charlie/apothecary_runs_", Sys.Date(), additional_identifier)
+context_name <- paste0("N:/Charlie/apothecary_runs_", Sys.Date(), additional_identifier)
 ctx <- context::context_save(path = context_name,
                              sources = sources,
                              packages = packages,
                              package_sources = provisionr::package_sources(local =
-                                                                             c("M:/Charlie/apothecary_0.1.0.zip",
-                                                                               "M:/Charlie/dde_1.0.2.zip",
-                                                                               "M:/Charlie/odin_1.0.6.zip",
-                                                                               "M:/Charlie/odin.js_0.1.8.zip",
-                                                                               "M:/Charlie/squire_0.4.35.zip")))
+                                                                             c("N:/Charlie/apothecary_0.1.0.zip",
+                                                                               "N:/Charlie/dde_1.0.2.zip",
+                                                                               "N:/Charlie/odin_1.0.6.zip",
+                                                                               "N:/Charlie/odin.js_0.1.8.zip",
+                                                                               "N:/Charlie/squire_0.4.35.zip")))
 
 # Configure the Queue
 run <- didehpc::queue_didehpc(ctx, config = config)
@@ -34,17 +35,76 @@ run$cluster_load(nodes = FALSE)
 run$task_list()
 run$task_times()
 
-# Tester
+# Testing the Running Locally
 pars_init <- readRDS("pars_init.rds")
 ecdc <- readRDS("ecdc_all.rds")
 interventions <- readRDS("google_brt.rds")
-test <- run$enqueue(run_apothecary_MCMC(pars_init, ecdc, interventions, 250, 1))
+countries <- unique(squire::population$iso3c)
 
-test$context_id()
-test$times()
-test$log()
-test$status()
-x <- test$result()
+missing_ISOs <- countries[!(countries %in% names(pars_init))]
+missing_countries <- squire::population$country[match(missing_ISOs, squire::population$iso3c)]
+included_countries <- countries[countries %in% names(pars_init)]
 
-dim(x$output)
+for (i in 1:length(included_countries)) {
+  test <- run_apothecary_MCMC(country = included_countries[i], pars_init = pars_init, ecdc = ecdc,
+                              interventions = interventions, n_mcmc = 2, run_identifier = 1)
+  print(i)
+}
+
+# Running the Fitting for Every Country
+for (i in 1:length(included_countries)) {
+  test <- run$enqueue(run_apothecary_MCMC(country = included_countries[i], pars_init = pars_init, ecdc = ecdc,
+                                          interventions = interventions, n_mcmc = 50000, run_identifier = 1))
+  print(i)
+}
+table(unname(run$task_status()), useNA = "ifany")
+
+# Rerunning the initial failures
+initial_failures <- which(unname(run$task_status() == "PENDING"))
+for (i in initial_failures) {
+  test <- run$enqueue(run_apothecary_MCMC(country = included_countries[i], pars_init = pars_init, ecdc = ecdc,
+                                          interventions = interventions, n_mcmc = 50000, run_identifier = 2))
+  print(i)
+}
+table(unname(run$task_status()), useNA = "ifany")
+
+# Finding which ones just haven't run then rerunning
+successful_countries <- substring(list.files("N:/Charlie/apothecary_run_results/"), first = 1, last = 3)
+table(successful_countries)
+failed_countries <- included_countries[!(included_countries %in% successful_countries)]
+for (i in 1:length(failed_countries)) {
+  test <- run$enqueue(run_apothecary_MCMC(country = failed_countries[i], pars_init = pars_init, ecdc = ecdc,
+                                          interventions = interventions, n_mcmc = 50000, run_identifier = 1))
+  print(failed_countries[i])
+}
+
+table(unname(run$task_status()))
+
+
+
+x <- run$task_times()
+
+
+non_cluster_included_countries <- included_countries[which(is.na(x$started))]
+non_cluster_ids <- x$task_id[which(is.na(x$started))]
+run$task_status(non_cluster_ids)
+
+which(unname(run$task_status() == "ERROR"))
+included_countries[78]
+run$task_status(run$task_list()[78])
+run$task_result(run$task_list()[78])
+
+
+x <- which(unname(run$task_status() == "PENDING"))
+run$task_status(run$task_list()[x])
+
+
+x <- run$task_times()
+sum(!is.na(x$started))
+x <- run$task_list()
+run$task_status()
+
+
+run$task_status("ba08bb572912d521c5234b1972991d9f")
+run$task_result("ba08bb572912d521c5234b1972991d9f")
 
